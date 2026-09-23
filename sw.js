2d94025fe1baf8837ed298a55fdc4f23a91d13ea
yeyee0910-coder <yeyee0910@gmail.com>
2026-09-23 16:29:25 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'mis-finanzas-v1';
+var APP_SHELL = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(APP_SHELL);
+    }).then(function () {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (key) {
+        return key !== CACHE_NAME;
+      }).map(function (key) {
+        return caches.delete(key);
+      }));
+    }).then(function () {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request).then(function (response) {
+        var copy = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, copy);
+        });
+        return response;
+      });
+    }).catch(function () {
+      return caches.match('./index.html');
+    })
+  );
+});
